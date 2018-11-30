aea2e4d35475222339f2a7a900b532fc8bc2d4d8
vinnyricciardi <vinnyricciardi@gmail.com>
2018-11-30 07:11:36 -0500

    MESSAGE

---
+++ content/projects/landgrabs/landgrabs.md
@@ -0,0 +1,19 @@
+Title: Land Reform and Tenure
+Slug: Land_Policy_Project
+Date: 2018-11-30
+Category:
+Tags: Project Description
+Author: Vinny Ricciardi
+Summary:
+
+## Mapping Global Land Reform and Tenure News
+
+<br><br>
+
+This map shows news articles coded as land tenure or land reform from January 2013 to October 2018. Articles were queried using the amazing [GDELT](https://www.gdeltproject.org/) project, which streams news articles from over 100 countries every 15 minutes and codes them into events, actors, locations, and sentiments (e.g., the tone of the article). This map, made with Google BigQuery and CartoDB, is in beta to give a picture of what GDELT can do for the land grab monitoring community.
+
+<br><br>
+
+<iframe width="100%" height="700" frameborder="0" src="https://vinnyricciardi.carto.com/builder/7c08d15d-cb86-4f7b-818e-151acc0ed4a0/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+
+<br><br>
--- pelicanconf.py
+++ pelicanconf.py
@@ -170,12 +170,11 @@ MENUITEMS = [
     ('CV', '/aboutme/curriculumvitae/'),
     ('Research Projects', [
         ('Farm Size', '/smallholders/Global_production/index.html'), #'/smallholders/Farm_Size_Project/index.html'),  # finish slideshow before activating
-        ('&nbsp&nbsp How much food do smallholders produce?', '/smallholders/Global_production/index.html'),
-        # ('&nbsp&nbsp Systematic Review', '/misc/Place_Holder/index.html'),        # To be replaced with jupyter notebook
-        # ('&nbsp&nbsp Climate Impacts', '/misc/Place_Holder/index.html'),          # To be replaced with jupyter notebook
+        # ('&nbsp&nbsp How much food do smallholders produce?', '/smallholders/Global_production/index.html'),
+        # ('&nbsp&nbsp Meta-Analysis', '/misc/Place_Holder/index.html'),        # To be replaced with jupyter notebook
         # ('&nbsp&nbsp Public Support', '/misc/Place_Holder/index.html'),           # To be replaced with jupyter notebook
-        ('Mountain Farming', '/mtnadaptation/Mtn_Adaptation_Project/index.html'), 
-        ('&nbsp&nbsp Climate adaptation in mountains: Are they enough?', '/mtnadaptation/Mtn_Adaptation_Project/index.html')    
+        ('Mountain Farming', '/mtnadaptation/Mtn_Adaptation_Project/index.html'),
+        ('Land Grabs', '/landgrabs/Land_Policy_Project/index.html')    
         ]),
     # ('Tools and Tricks', '/category/toolsntricks.html'),
     ('News', '/news/news/index.html')
